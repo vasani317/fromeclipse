185aa8b26b1634e17a6585655758c04bd18871ae
HP <HP@DESKTOP-4002SIB>
2020-07-15 06:20:17 -0700

    First Commit Uploading files

---
+++ Demo/src/Hello_World.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author HP
+ *
+ */
+public class Hello_World {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
